ab12408d1b4361818d0d7a172868b6bbeecfb382
Yixi <lyh1112@gmail.com>
2013-07-18 21:48:55 +0800

    Initial Commit

---
+++ QuickNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuickNote
+//
+//  Created by Yixi Liu on 13-7-18.
+//  Copyright (c) 2013年 Yixi Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YXAppDelegate class]));
+    }
+}
